d6be40ab57b72a89f28341a99cdbc92ae3fa0bb7
FVitaleSF <129391181+FVitaleSF@users.noreply.github.com>
2024-12-15 18:34:33 +0000

    Auth lifecycle

---
+++ pages/login.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from salesforce_auth import salesforce_auth
+
+
+endpoint = st.text_input('Add endpoint',placeholder="your_domain")
+client_id = st.text_input('Add Client Id')
+client_secret = st.text_input('Add Client Secret')
+
+def authenticate():
+    if not endpoint or not client_id or not client_secret:
+        st.error("All fields are required!")
+        return
+    try:
+        params = salesforce_auth(endpoint,client_id,client_secret)
+        st.session_state.access_token = params.authorize_service()      
+        st.session_state.authenticated = True
+        st.success('You succesfully logged in!')
+
+    except Exception as e:
+        st.session_state.authenticated = False
+        st.error(f"An error occurred {e}")
+
+st.button('Authorize',on_click=authenticate)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit as st
-from salesforce_auth import salesforce_auth
+
+DEFAULT_PAGE = [st.Page('pages/login.py')]
+AUTH_PAGES = [st.Page('pages/records_get.py')]
 
 
-st.title('Authorize to Salesforce')
+def logout():
+    st.session_state.access_token = None
+    st.session_state.authenticated = False
 
-endpoint = st.text_input('Add endpoint',placeholder="your_domain")
-client_id = st.text_input('Add Client Id')
-client_secret = st.text_input('Add Client Secret')
+if 'display_pages' not in st.session_state:
+    display_pages = DEFAULT_PAGE.copy()
 
 if 'authenticated' not in st.session_state:
     st.session_state.authenticated = False
@@ -14,20 +17,12 @@ if 'authenticated' not in st.session_state:
 if 'access_token' not in st.session_state:
     st.session_state.access_token = None
 
-def authenticate():
-    if not endpoint or not client_id or not client_secret:
-        st.error("All fields are required!")
-        return
-    try:
-        params = salesforce_auth(endpoint,client_id,client_secret)
-        st.session_state.access_token = params.authorize_service()      
-        st.session_state.authenticated = True
+if st.session_state.authenticated:
+    display_pages = AUTH_PAGES.copy()
+    st.sidebar.button('Logout',on_click=logout)
 
-    except Exception as e:
-        st.session_state.authenticated = False
-        st.error(f"An error occurred {e}")
+    
 
-st.button('Authorize',on_click=authenticate)
 
-if 'authenticated' in st.session_state:
-    st.navigation([st.Page('records_get.py')])
+pg = st.navigation(display_pages,position="sidebar")
+pg.run()
